fad8d91e36af8751feb747529a7b3229b1122cd9
Natasha Murashev <nmurashev@gmail.com>
2013-05-10 08:25:47 -0500

    tracking when a game ends (all cards are matched)

--- MemoryGameAdvanced/ViewController.m
+++ MemoryGameAdvanced/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()
 
 @property NSMutableArray *openCards;
+@property NSInteger numberOfCardViewsDisplayed;
 
 @end
 
@@ -23,9 +24,12 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    self.numberOfCardViewsDisplayed = 0;
+    
     for (UIView *__view in self.view.subviews) {
         if ([__view isKindOfClass:[CardView class]]) {
             ((CardView *)(__view)).delegate = self;
+            self.numberOfCardViewsDisplayed ++;
         }
     }
     
@@ -42,21 +46,23 @@
 {
     
     if (self.openCards.count < 2) {
-        
         [cardView showCard];
         [self.openCards addObject:cardView];
         
         if (self.openCards.count == 2) {
-            
             if (((CardView *)(self.openCards[0])).tag == ((CardView *)(self.openCards[1])).tag) {
-                NSLog(@"Match Found!!!!");
                 for (CardView *card in self.openCards) {
                     [card eliminateCard];
+                    self.numberOfCardViewsDisplayed--;
+                }
+                
+                if (self.numberOfCardViewsDisplayed == 0) {
+                    NSLog(@" YOU WON!!!!");
+                    [self reloadInputViews];
                 }
             }
         }
     } else {
-        
         for (CardView *card in self.openCards) {
             [card hideCard];
         }
